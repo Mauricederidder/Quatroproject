91ad99a8798530650a85695b008c625d46ec568e
Maurice <mauricederidder@outlook.com>
2020-12-15 17:06:57 +0100

    Domain - Create crud interface

---
+++ Project/src/domain/Crud.java
@@ -0,0 +1,17 @@
+package domain;
+
+import java.util.List;
+
+public interface Crud<T>{
+    // T mag je invullen bij het implementeren van een class die Crud gaat gebruiken.
+    // Bijvoorbeeld: implements Crud<Person> voor de Person class 😎
+    public void create(T params);
+
+    public void delete(int id);
+    // specifiek 1 entiteit terug
+    public T get(int id);
+    // overflow 🌊 methode om alles terug te krijgen 
+    public T get();
+
+    public void update(int id,T params);
+}
