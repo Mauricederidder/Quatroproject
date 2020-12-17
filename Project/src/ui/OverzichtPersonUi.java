71ca0eba1b92c246f38317cbb786a5a2418d9443
luukluuk34 <luuk-bartels@hotmail.com>
2020-12-17 16:28:25 +0100

    ui OverzichtPersonUi button overzicht
    Added functionality to the button overzicht so that you can open the window there.

--- Project/src/ui/OverzichtPersonUi.java
+++ Project/src/ui/OverzichtPersonUi.java
@@ -11,21 +11,31 @@ import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+import ui.CreatePersonUi;
 
 public class OverzichtPersonUi extends Application {
 
     @Override
     public void start(Stage window) {
+        CreatePersonUi createPerson = new CreatePersonUi();
 
         BorderPane layout = new BorderPane();
-
         VBox vertical = new VBox();
-
         vertical.getChildren().add(new Label("Overzicht personen"));
+
+        Button overzicht = new Button("Overzicht");
+        overzicht.setOnAction((event) -> {
+           // Stage stageOverzicht = new Stage();
+            try {
+                createPerson.startCreatePerson();
+            } catch (Exception E) {
+                System.out.println("Something whent wrong!");
+            }
+        });
 
         HBox texts = new HBox();
         texts.setSpacing(30);
-        texts.getChildren().add(new Button("Overzicht"));
+        texts.getChildren().add(overzicht);
         texts.getChildren().add(new Button("Aanmaken"));
         texts.getChildren().add(new Button("Updaten"));
         texts.getChildren().add(new Button("Verwijderen"));
