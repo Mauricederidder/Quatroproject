e4ad0797af59df044c95d63ef79151eab1491264
luukluuk34 <luuk-bartels@hotmail.com>
2020-12-17 13:48:13 +0100

    ui update WIP
    Added imports to javafx

--- Project/src/ui/OverzichtPersonUi.java
+++ Project/src/ui/OverzichtPersonUi.java
@@ -1,5 +1,19 @@
 package ui;
 
-public class OverzichtPersonUi {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.FlowPane;
+import javafx.stage.Stage;
+
+public class OverzichtPersonUi extends Application {
+
+
+    
+    @Override
+    public void start(Stage arg0) throws Exception {
+        // TODO Auto-generated method stub
+
+    }
     
 }
