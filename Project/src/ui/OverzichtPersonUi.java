75306def108481f63f9bd36449cf84087e26f55a
luukluuk34 <luuk-bartels@hotmail.com>
2020-12-20 22:04:36 +0100

    ui OverzichtPersonUi Delete
    Deleted 2 weird imports

--- Project/src/ui/OverzichtPersonUi.java
+++ Project/src/ui/OverzichtPersonUi.java
@@ -6,10 +6,8 @@ import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
-import javafx.scene.layout.TilePane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
