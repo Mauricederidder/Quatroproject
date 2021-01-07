17ba705e7bbfc54c1324d393f43a24da056b4867
luukluuk34 <luuk-bartels@hotmail.com>
2021-01-07 11:27:43 +0100

    UI CodeCademyAllCoursesScene add
    Added codeCademyAllCoursesScene

---
+++ Project/src/ui/CodeCademyAllCoursesScene.java
@@ -0,0 +1,25 @@
+package ui;
+
+
+import javafx.application.Application;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.stage.Stage;
+import javafx.scene.effect.*;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.*;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
+
+public class CodeCademyAllCoursesScene {
+    
+    protected static void CodeCademyAllCoursesSceneBuilder(){
+        getStage().setScene(OverviewPersonScene.overviewPersonScene());;
+
+    }
+}
