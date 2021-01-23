99d4f6cda1d671eb22cd78db825baac51c64ee97
luukluuk34 <luuk-bartels@hotmail.com>
2021-01-23 10:39:39 +0100

    added event button and the logic for add module

--- Project/src/ui/codeCademyAddModule.java
+++ Project/src/ui/codeCademyAddModule.java
@@ -22,7 +22,7 @@ import javafx.scene.text.Text;
 
 public class codeCademyAddModule {
 
-    protected static Scene CodeCademyAddModuleSceneBuilder(){
+    protected static Scene CodeCademyAddModuleSceneBuilder() {
         Scene codeCademyAllCoursesScene = new Scene(codeCademyAddModuleLayout());
         return codeCademyAllCoursesScene;
     }
@@ -35,7 +35,7 @@ public class codeCademyAddModule {
         return root;
     }
 
-    protected static FlowPane codeCademyAddModuleCenterLayout(){
+    protected static FlowPane codeCademyAddModuleCenterLayout() {
         FlowPane rootToo = new FlowPane();
         rootToo.setVgap(10);
         rootToo.setHgap(10);
@@ -44,76 +44,86 @@ public class codeCademyAddModule {
         return rootToo;
     }
 
-    protected static GridPane codeCademyAddModuleGridHeader(){
+    protected static GridPane codeCademyAddModuleGridHeader() {
         GridPane headerGrid = new GridPane();
         Label emptyLabel = new Label();
-        headerGrid.setPadding(new Insets(30,10,50,10));
+        headerGrid.setPadding(new Insets(30, 10, 50, 10));
         headerGrid.setHgap(30);
-        headerGrid.add(emptyLabel,0,1);
-        headerGrid.add(codeCademyAddModuleSceneHeader(),0,2);
+        headerGrid.add(emptyLabel, 0, 1);
+        headerGrid.add(codeCademyAddModuleSceneHeader(), 0, 2);
         headerGrid.setAlignment(Pos.TOP_CENTER);
         return headerGrid;
     }
-    
-    protected static VBox codeCademyAddModuleSceneHeader(){
+
+    protected static VBox codeCademyAddModuleSceneHeader() {
         VBox headerVbox = new VBox();
         headerVbox.getChildren().add(codeCademyAddModuleHeaderTitle());
         return headerVbox;
     }
 
-    protected static Label codeCademyAddModuleHeaderTitle(){
+    protected static Label codeCademyAddModuleHeaderTitle() {
         Label title = new Label("Add Module");
-        title.setFont(Font.loadFont(CodeCademyStage.class.getResource("Montserrat-Bold.ttf").toExternalForm(),40));
+        title.setFont(Font.loadFont(CodeCademyStage.class.getResource("Montserrat-Bold.ttf").toExternalForm(), 40));
         return title;
     }
 
     protected static VBox codeCademyAddModuleTextfieldVbox(){
         VBox vbox = new VBox();
-        vbox.getChildren().add(codeCademyAddModuleTextFieldLabelCourseName());
-        vbox.getChildren().add(codeCademyAddModuleTextFieldCourseName());
+        TextField textFieldModuleName = codeCademyAddModuleTextFieldModuleName(); 
+        TextField TextFieldModuletag = codeCademyAddModuleTextFieldModuleTag() ;
+        Button addModuleButton = codeCademyAddModuleButton();
+
+        vbox.getChildren().add(codeCademyAddModuleTextFieldLabelModuleName());
+        vbox.getChildren().add(textFieldModuleName);
         vbox.getChildren().add(new Label(""));
-        vbox.getChildren().add(codeCademyAddModuleTextFieldLabelCourseTag());
-        vbox.getChildren().add(codeCademyAddModuleTextFieldCourseTag());
+        vbox.getChildren().add(codeCademyAddModuleTextFieldLabelModuleTag());
+        vbox.getChildren().add(TextFieldModuletag);
         vbox.getChildren().add(new Label(""));
         vbox.getChildren().add(codeCademyAddModuleButton());
+        vbox.getChildren().add(addModuleButton);
+        String name = textFieldModuleName.getText();
+        String tag = TextFieldModuletag.getText();
+        addModuleButton.addEventHandler(MouseEvent.MOUSE_CLICKED, codeCademyAddModuleLogic.eventHandlerMouseClickedAddModule(name,tag));
         return vbox;
     }
-    protected static Label codeCademyAddModuleTextFieldLabelCourseName(){
+
+    protected static Label codeCademyAddModuleTextFieldLabelModuleName() {
         Label TextFieldLabelOne = new Label("Module name: ");
-        TextFieldLabelOne.setFont(Font.loadFont(CodeCademyStage.class.getResource("Montserrat-Bold.ttf").toExternalForm(),16));
+        TextFieldLabelOne
+                .setFont(Font.loadFont(CodeCademyStage.class.getResource("Montserrat-Bold.ttf").toExternalForm(), 16));
         TextFieldLabelOne.setTextFill(Color.web("#000000", 1.0));
         return TextFieldLabelOne;
     }
 
-    protected static TextField codeCademyAddModuleTextFieldCourseName(){
-        TextField TextFieldCoursename = new TextField();
-        TextFieldCoursename.setText("Module name");
-        return TextFieldCoursename;
+    protected static TextField codeCademyAddModuleTextFieldModuleName() {
+        TextField TextFieldModulename = new TextField();
+        TextFieldModulename.setText("Module name");
+        return TextFieldModulename;
     }
 
-    protected static Label codeCademyAddModuleTextFieldLabelCourseTag(){
+    protected static Label codeCademyAddModuleTextFieldLabelModuleTag() {
         Label TextFieldLabelOne = new Label("Module tags: ");
-        TextFieldLabelOne.setFont(Font.loadFont(CodeCademyStage.class.getResource("Montserrat-Bold.ttf").toExternalForm(),16));
+        TextFieldLabelOne
+                .setFont(Font.loadFont(CodeCademyStage.class.getResource("Montserrat-Bold.ttf").toExternalForm(), 16));
         TextFieldLabelOne.setTextFill(Color.web("#000000", 1.0));
         return TextFieldLabelOne;
     }
 
-    protected static TextField codeCademyAddModuleTextFieldCourseTag(){
-        TextField TextFieldCoursename = new TextField();
-        TextFieldCoursename.setText("Module tags");
-        return TextFieldCoursename;
+    protected static TextField codeCademyAddModuleTextFieldModuleTag() {
+        TextField TextFieldModuleTag = new TextField();
+        TextFieldModuleTag.setText("Module tags");
+        return TextFieldModuleTag;
     }
 
-    protected static Button codeCademyAddModuleButton(){
+    protected static Button codeCademyAddModuleButton() {
         Button button = new Button("Add Module");
         button.setStyle("-fx-background-color:#000000");
-        button.setFont(Font.loadFont(CodeCademyStage.class.getResource("Montserrat-Bold.ttf").toExternalForm(),16));
-        button.setTextFill(Color.web("#FFFFFF"));;
+        button.setFont(Font.loadFont(CodeCademyStage.class.getResource("Montserrat-Bold.ttf").toExternalForm(), 16));
+        button.setTextFill(Color.web("#FFFFFF"));
+        ;
         button.setMaxHeight(50);
         button.setMaxWidth(450);
         return button;
     }
 
 }
-
-
---
+++ Project/src/ui/codeCademyAddModuleLogic.java
@@ -0,0 +1,32 @@
+package ui;
+
+import javafx.application.Application;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.stage.Stage;
+import javafx.scene.effect.*;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.*;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
+
+public class codeCademyAddModuleLogic {
+    
+
+    protected static EventHandler<MouseEvent> eventHandlerMouseClickedAddModule(String name, String tag) {
+        EventHandler<MouseEvent> eventHandlerMouseClick = new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent e) {
+                
+                
+
+            }
+        };
+        return eventHandlerMouseClick;
+    }
+
+}
